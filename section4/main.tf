2b029aaaa2e7188dc2f1f324512e8ab2cc6b0643
agus-mac-031 <benochson@arisglobal.com>
2023-09-03 21:19:37 -0400

    typo

--- section4/main.tf
+++ section4/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "section3_vm" {
+resource "aws_instance" "section4_vm" {
   ami           = var.ami //Ubuntu AMI
   instance_type = var.instance_type
 
